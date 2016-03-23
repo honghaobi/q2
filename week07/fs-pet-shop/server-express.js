0c41008e9b1946c929e2962e61a4e2f2370ed842
henrybi <henrybi@u.washington.edu>
2016-03-23 13:45:00 -0700

    finished express assignement

---
+++ week07/fs-pet-shop/server-express.js
@@ -0,0 +1,61 @@
+var http = require('http');
+var url = require('url');
+
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+
+var fs = require('fs');
+var path = require('path');
+var petsPath = path.join(__dirname, 'pets.json');
+
+var pets;
+
+fs.readFile(petsPath, 'utf8', (err, data) => {
+  pets = JSON.parse(data);
+});
+
+function postData(){
+  var petsJSON = JSON.stringify(pets);
+
+  fs.writeFile(petsPath, petsJSON, (writeErr) => {
+    if (writeErr) {
+      throw writeErr;
+    }
+  });
+};
+
+app.post('/pets', function(req,res){
+      var age = parseInt(req.body.age);
+      var kind = req.body.kind;
+      var name = req.body.name;
+
+      pets.push({ age, kind, name});
+      postData();
+      res.send(pets);
+});
+
+app.get('/pets', function(req, res){
+  res.send('here are your pets ' + JSON.stringify(pets));
+});
+
+app.get('/pets/:index', function(req, res) {
+  var index = req.params.index;
+
+  if (isNaN(index) || index >= pets.length || index < 0) {
+    res.status(404).send('invalid index');
+  } else{
+    res.send('here is your pet ' + JSON.stringify(pets[index]));
+  }
+});
+
+app.get('/*', function (req, res) {
+  res.status(404).send('nope! nothing here.');
+});
+
+// start the server
+app.listen(8080, function() {
+  console.log("Starting a server on localhost:8080");
+});
