fbefab64dbfa02994f4671ffa4421d488f7fa611
henrybi <henrybi@u.washington.edu>
2016-04-04 14:37:21 -0700

    finished assignment

--- week09/intro-to-express-validations-EXERCISE-2/validation2/app.js
+++ week09/intro-to-express-validations-EXERCISE-2/validation2/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -7,9 +8,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var flash = require('connect-flash');
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,9 +21,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(session({secret: "testsecret", cookie: { maxAge: 60000 }}));
+
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
+
+app.use(flash());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- week09/intro-to-express-validations-EXERCISE-2/validation2/routes/index.js
+++ week09/intro-to-express-validations-EXERCISE-2/validation2/routes/index.js
@@ -1,33 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
+
 function Users() {
   return knex('users');
 }
 
-/* GET home page. */
+var errors = [];
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {messages: errors});
 });
 
 router.get('/new', function(req, res, next) {
-  res.render('new');
+  res.render('new', {messages: errors});
 });
 
 router.post('/new', function(req, res, next) {
-  var errors = [];
-
+  errors = [];
   if (req.body.name && req.body.hobby) {
     Users().insert({name: req.body.name, hobby: req.body.hobby}).then(function () {
+      req.flash("success", "user added successfully");
+      res.redirect('/people');
+    }).catch(function(error){
+      req.flash("fail", "user has been added");
       res.redirect('/people');
     });
   } else {
-    if (!req.body.name) {
-      errors.push("enter yo name yo!");
-    }
-    if (!req.body.hobby) {
-      errors.push("enter yo hobby yo!");
-    }
+    !req.body.name ? errors.push('enter yo name yo!') : null;
+    !req.body.hobby ? errors.push('enter yo hobby yo!') : null;
     res.render('new', {messages: errors});
   }
 });
