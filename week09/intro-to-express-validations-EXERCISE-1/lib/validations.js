58111a1179ec7666ddde24364abfbc6567e8d624
henrybi <henrybi@u.washington.edu>
2016-04-04 11:26:29 -0700

    finished stretch goal

--- week09/intro-to-express-validations-EXERCISE-1/lib/validations.js
+++ week09/intro-to-express-validations-EXERCISE-1/lib/validations.js
@@ -3,9 +3,11 @@ module.exports = {
     return !input.trim() ? 'Name cannot be blank' : '';
   },
   emailIsValid: function (input) {
-    return !input.trim() ? 'Email is invalid' : '';
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return !re.test(input) ? 'Email is invalid' : '';
   },
   phoneIsValid: function (input) {
-    return !input.trim() ? 'Phone number is invalid' : '';
+    var re = /\d{3}\-*\d{3}\-*\d{4}/;
+    return !re.test(input) ? 'Phone number is invalid' : '';
   }
 }
--- week09/intro-to-express-validations-EXERCISE-1/test/test_stretch_user_validations.js
+++ week09/intro-to-express-validations-EXERCISE-1/test/test_stretch_user_validations.js
@@ -5,7 +5,7 @@ var validate = require('../lib/validations');
 
 describe('User Validations', function () {
   describe('Name is Valid', function() {
-    xit('Name cannot be blank', function (done) {
+    it('Name cannot be blank', function (done) {
       validate.nameIsNotBlank.should.be.a('function');
       assert.equal(validate.nameIsNotBlank('   '), 'Name cannot be blank');
       assert.equal(validate.nameIsNotBlank('George Orwell'), '');
@@ -13,41 +13,41 @@ describe('User Validations', function () {
     });
   });
   describe('Email is Valid', function () {
-    xit('should not be blank', function (done) {
+    it('should not be blank', function (done) {
       validate.emailIsValid.should.be.a('function');
       assert.equal(validate.emailIsValid('    '), 'Email is invalid')
       assert.equal(validate.emailIsValid('me@me.com'), '')
       done();
     });
-    xit('should not be just @ symbol', function (done) {
+    it('should not be just @ symbol', function (done) {
       assert.equal(validate.emailIsValid('@'), 'Email is invalid')
       assert.equal(validate.emailIsValid('me@me.it'), '')
       done();
     });
-    xit('should have text before @ symbol', function (done) {
+    it('should have text before @ symbol', function (done) {
       assert.equal(validate.emailIsValid('@i.com'), 'Email is invalid')
       assert.equal(validate.emailIsValid('you@i.it'), '')
       done();
     });
-    xit('should have valid text after @ symbol', function (done) {
+    it('should have valid text after @ symbol', function (done) {
       assert.equal(validate.emailIsValid('hello@'), 'Email is invalid')
       assert.equal(validate.emailIsValid('@i.c'), 'Email is invalid')
       assert.equal(validate.emailIsValid('me@me.com'), '')
       done();
     });
     describe('Phone Number is Valid', function () {
-      xit('should not be blank', function (done) {
+      it('should not be blank', function (done) {
         validate.phoneIsValid.should.be.a('function');
         assert.equal(validate.phoneIsValid('  '), 'Phone number is invalid');
         done();
       });
-      xit('should have enough digits', function (done) {
+      it('should have enough digits', function (done) {
         validate.phoneIsValid.should.be.a('function');
         assert.equal(validate.phoneIsValid('123555234'), 'Phone number is invalid');
         assert.equal(validate.phoneIsValid('1235552345'), '');
         done();
       });
-      xit('should accept dashes in input', function (done) {
+      it('should accept dashes in input', function (done) {
         validate.phoneIsValid.should.be.a('function');
         assert.equal(validate.phoneIsValid('123-555-2345'), '');
         done();
