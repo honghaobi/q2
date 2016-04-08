da72bf0429f99fedd7e9cce96fab8960771ab1fc
henrybi <henrybi@u.washington.edu>
2016-04-08 16:58:45 -0700

    author with books work!!!

--- galvanize-reads/routes/index.js
+++ galvanize-reads/routes/index.js
@@ -19,15 +19,37 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+// router.get('/authors', function(req, res, next) {
+//   Authors().join('author_book_relationship', 'authors.id', 'author_book_relationship.author_id').join('books', 'books.id', 'author_book_relationship.book_id').select().then(function (authors) {
+//     res.render('authors', {authors});
+//   });
+// });
+
 router.get('/authors', function(req, res, next) {
-  Authors().select().then(function (authors) {
-    res.render('authors', {allAuthors: authors});
-  });
+
+  var getBooksByAuthor = function(authorId) {
+    return Authors().select().first().where({id:authorId}).then(function(author){
+      return Authors().join('author_book_relationship', 'authors.id', 'author_book_relationship.author_id').join('books', 'books.id', 'author_book_relationship.book_id').select().where({'authors.id': authorId}).then(function (booksByAuthor) {
+        return {author, booksByAuthor} ;
+      });
+    });
+  };
+
+    var uniqueAuthorIdArray = [];
+
+    Authors().pluck('id').then(function (authorIdArray) {
+      uniqueAuthorIdArray = authorIdArray;
+      var booksByAuthor = authorIdArray.map(getBooksByAuthor);
+      Promise.all(booksByAuthor).then(function(finalBooksByAuthor){
+        console.log(finalBooksByAuthor);
+        res.render('authors', {finalBooksByAuthor});
+      });
+    });
 });
 
 router.get('/books', function(req, res, next) {
-  Books().select().then(function (books) {
-    res.render('books', {allBooks: books});
+  Books().join('author_book_relationship', 'books.id', 'author_book_relationship.book_id').join('authors', 'authors.id', 'author_book_relationship.author_id').select().then(function (books) {
+    res.render('books', {books});
   });
 });
 
