4ea73508813185cab953af50c2c74d8a4bfdef27
henrybi <henrybi@u.washington.edu>
2016-04-09 16:13:59 -0700

    client side addeding selected author removing selected author in add author page feature.

--- galvanize-reads/public/javascripts/client.js
+++ galvanize-reads/public/javascripts/client.js
@@ -4,4 +4,14 @@ $(document).ready(function() {
     $(this).toggleClass('text-show');
   });
 
+  $('.addAuthor').on('click', function () {
+      var authorID = $('.authorSelection').val();
+      var authorName = $('.authorSelection option:selected').text();
+      $('.addedAuthor').append("<option value=" + authorID + ">" + authorName + "</option>");
+    });
+
+  $('.removeAuthor').on("click", function () {
+      $('.addedAuthor option:selected').remove();
+  });
+
 });
