6cd1dff352a371a6ae2af441bc507c7d95a801c9
henrybi <henrybi@u.washington.edu>
2016-04-09 14:12:30 -0700

    added individual author or book routes

--- galvanize-reads/routes/index.js
+++ galvanize-reads/routes/index.js
@@ -14,21 +14,28 @@ function ABR() {
   return knex('author_book_relationship');
 }
 
+function getBooksByAuthor (authorId) {
+  return Authors().select().first().where({id:authorId}).then(function(author){
+    return Authors().join('author_book_relationship', 'authors.id', 'author_book_relationship.author_id').join('books', 'books.id', 'author_book_relationship.book_id').select().where({'authors.id': authorId}).then(function (booksByAuthor) {
+      return {author, booksByAuthor} ;
+    });
+  });
+};
+
+function getAuthorsByBook (bookId) {
+  return Books().select().first().where({id:bookId}).then(function(book){
+    return Books().join('author_book_relationship', 'books.id', 'author_book_relationship.book_id').join('authors', 'authors.id', 'author_book_relationship.author_id').select().where({'books.id': bookId}).then(function (authorsByBook) {
+      return {book, authorsByBook} ;
+    });
+  });
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
 router.get('/authors', function(req, res, next) {
-
-  var getBooksByAuthor = function(authorId) {
-    return Authors().select().first().where({id:authorId}).then(function(author){
-      return Authors().join('author_book_relationship', 'authors.id', 'author_book_relationship.author_id').join('books', 'books.id', 'author_book_relationship.book_id').select().where({'authors.id': authorId}).then(function (booksByAuthor) {
-        return {author, booksByAuthor} ;
-      });
-    });
-  };
-
   Authors().pluck('id').then(function (authorsIdArray) {
     var booksByAuthor = authorsIdArray.map(getBooksByAuthor);
     Promise.all(booksByAuthor).then(function(renderBooksByAuthor){
@@ -38,21 +45,34 @@ router.get('/authors', function(req, res, next) {
 });
 
 router.get('/books', function(req, res, next) {
-
-  var getAuthorsByBook = function(bookId) {
-    return Books().select().first().where({id:bookId}).then(function(book){
-      return Books().join('author_book_relationship', 'books.id', 'author_book_relationship.book_id').join('authors', 'authors.id', 'author_book_relationship.author_id').select().where({'books.id': bookId}).then(function (authorsByBook) {
-        return {book, authorsByBook} ;
-      });
-    });
-  };
-
   Books().pluck('id').then(function (booksIdArray) {
     var authorsByBook = booksIdArray.map(getAuthorsByBook);
     Promise.all(authorsByBook).then(function(renderAuthorsByBook){
       res.render('books', {renderAuthorsByBook});
     });
   });
+});
+
+router.get('/books/:id', function(req, res, next) {
+  Promise.resolve(getAuthorsByBook(req.params.id)).then(function(renderAuthorByBook){
+    res.render('book', {renderAuthorByBook});
+  });
+});
+
+router.get('/authors/:id', function(req, res, next) {
+  Promise.resolve(getBooksByAuthor(req.params.id)).then(function(renderBookByAuthor){
+    res.render('author', {renderBookByAuthor});
+  });
+});
+
+router.get('/books/create', function(req, res, next) {
+
+  res.render('create-book',{});
+});
+
+router.get('/authors/create', function(req, res, next) {
+
+  res.render('create-author',{});
 });
 
 module.exports = router;
