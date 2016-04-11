4f0b70fe4f4f7d2b480063d55603c0ac9e9d774d
henrybi <henrybi@u.washington.edu>
2016-04-11 10:57:23 -0700

    finished notes

--- week10/express-middleware-auth/routes/stuff.js
+++ week10/express-middleware-auth/routes/stuff.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+function myAccountRequired(req, res, next) {
+  if (req.session.user.id.toString() === req.params.id) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+}
+
+router.use((req, res, next) => {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+});
+
 router.get('/:id/regularStuff', (req, res, next) => {
   res.render('users/regularStuff')
 });
 
-router.get('/:id/specialStuff', (req, res, next) => {
+router.get('/:id/specialStuff', myAccountRequired, (req, res, next) => {
   res.render('users/specialStuff')
 });
 
